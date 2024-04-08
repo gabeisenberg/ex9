2d6562ffc3c88d50eeb293f3ff8f7757d3104455
gabeisenberg <g.isenberg@ufl.edu>
2024-04-07 22:22:20 -0400

    edits

--- client.c
+++ client.c
@@ -8,7 +8,6 @@
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
         return 1;
     }
 
@@ -17,44 +16,32 @@ int main(int argc, char *argv[]) {
     struct sockaddr_in servaddr;
     char buffer[MAX_MSG_SIZE];
 
-    // Create socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd == -1) {
-        perror("Socket creation failed");
         return 1;
     }
 
-    // Initialize server address
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); // Server running on localhost
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
     servaddr.sin_port = htons(port);
 
-    // Connect to server
     if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) != 0) {
-        perror("Connection to server failed");
         return 1;
     }
 
-    // Send message
-    char message[] = "<Your Name>: <UFID>";
+    char message[] = "Gabriel Isenberg: 27781379";
     if (send(sockfd, message, strlen(message), 0) == -1) {
-        perror("Message sending failed");
         return 1;
     }
 
-    // Receive message
     ssize_t bytes_received = recv(sockfd, buffer, MAX_MSG_SIZE - 1, 0);
     if (bytes_received == -1) {
-        perror("Message receiving failed");
         return 1;
     }
     buffer[bytes_received] = '\0';
 
-    printf("Server says: %s\n", buffer);
-
-    // Close socket
+    printf(buffer);
     close(sockfd);
-
     return 0;
 }
--- server.c
+++ server.c
@@ -9,70 +9,51 @@
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
         return 1;
     }
-
     int port = atoi(argv[1]);
     int listenfd, connfd;
     struct sockaddr_in servaddr, cliaddr;
     char buffer[MAX_MSG_SIZE];
 
-    // Create socket
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
     if (listenfd == -1) {
-        perror("Socket creation failed");
         return 1;
     }
-
-    // Initialize server address
+    
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(port);
 
-    // Bind socket to address
     if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) != 0) {
-        perror("Socket bind failed");
         return 1;
     }
 
-    // Listen for connections
     if (listen(listenfd, BACKLOG) != 0) {
-        perror("Listen failed");
         return 1;
     }
 
-    printf("Server listening on port %d...\n", port);
-
-    // Accept incoming connections
     socklen_t cliaddr_len = sizeof(cliaddr);
     connfd = accept(listenfd, (struct sockaddr *)&cliaddr, &cliaddr_len);
     if (connfd == -1) {
-        perror("Connection accept failed");
         return 1;
     }
 
-    // Receive message
     ssize_t bytes_received = recv(connfd, buffer, MAX_MSG_SIZE - 1, 0);
     if (bytes_received == -1) {
-        perror("Message receiving failed");
         return 1;
     }
     buffer[bytes_received] = '\0';
 
-    printf("Client says: %s\n", buffer);
+    printf(buffer);
 
-    // Send message
     char message[] = "Welcome to the server running on REPTILIAN";
     if (send(connfd, message, strlen(message), 0) == -1) {
-        perror("Message sending failed");
         return 1;
     }
 
-    // Close sockets
     close(connfd);
     close(listenfd);
-
     return 0;
 }
